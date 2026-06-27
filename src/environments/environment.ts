73c529c03573876be38b94e4d8bdb8f145c9e942
sivakumarmarisetti <marisettisivakumar2000@gmail.com>
2026-06-27 12:17:25 +0530

    commented local api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://d223juhqt63axh.cloudfront.net/api/v1'   // relative URL — works with the proxy we configure next  // relative URL — works with the proxy we configure next
+  // apiUrl: 'http://localhost:8080/api/v1'
 };
